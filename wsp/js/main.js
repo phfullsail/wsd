36104c5d57def4f61f96c714fb37be2ce7b47a60
Pamela Holmes <phfullsail@fullsail.edu>
2014-09-21 19:49:59 -0500

    wsp-polished

--- wsp/js/main.js
+++ wsp/js/main.js
@@ -5,9 +5,63 @@
 
 (function($){
 
+/************** Home navigation ***************/
+	$(".linkHome").click(function(e) {
+		e.preventDefault();
+		window.location.assign("index.html");
+	});
+
+/************** Spots navigation ***************/	
+	$(".linkSpots").click(function(e) {
+		e.preventDefault();
+		window.location.assign("spots.html");
+	});
+
+/************* Search Button *******************/
+	$("#btnFilters").click(function(e) {
+		e.preventDefault();
+		window.location.assign("results.html");
+	});
+
+	$(".imgKidParty").click(function(e) {
+		e.preventDefault();
+		window.location.assign("results.html");
+	});
 
 
-});
+	$(".result-detail-active").click(function(e) {
+		e.preventDefault();
+		window.location.assign("details.html");
+	});
+
+	$("#pirateLink").click(function(e) {
+		e.preventDefault();
+		window.location.assign("details.html");
+	});
+
+	$(".pinterest-link").click(function(e) {
+		e.preventDefault();
+		window.open("http://www.pinterest.com/", "_blank");
+	});
+
+	$(".twitter-link").click(function(e) {
+		e.preventDefault();
+		window.open("https://twitter.com/?lang=en", "_blank");
+	});
+
+
+	$(".fb-link").click(function(e) {
+			e.preventDefault();
+			window.open("https://www.facebook.com/", "_blank");
+	});
+
+	$(".pinterest-link").mouseover(function() {
+		
+	}) 
+
+
+	/********** Enable Bootstrap-Select ************/
+	 $('.selectpicker').selectpicker();
 
 	
-	
+})(jQuery); // end private scope
